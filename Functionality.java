751f6a7d63ee4b2bbab7bba0053f6d5633e15a88
Atul <patilatul179@gmail.com>
2024-05-07 10:36:04 +0530

    v1.2

--- Functionality.java
+++ Functionality.java
@@ -3,7 +3,7 @@ public class Functionality {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Main functionality going on advanced to v1.1");
+		System.out.println("Main functionality going on advanced to v1.2");
 
 	}
 
