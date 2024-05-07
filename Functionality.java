1d420f84dc6f8c89fd9e347581441d9aa4a8bbc6
Atul <patilatul179@gmail.com>
2024-05-07 10:37:27 +0530

    login func

--- Functionality.java
+++ Functionality.java
@@ -5,6 +5,8 @@ public class Functionality {
 		// TODO Auto-generated method stub
 
 		System.out.println("Main functionality going on");
+		
+		System.out.println("Login functionality going on");
 
 
 	}
